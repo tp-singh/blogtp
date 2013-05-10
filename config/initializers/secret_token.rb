67b5a697774ab49eaf30154a569e1d76916139d6
enbake <enbake@ubuntu.(none)>
2013-05-11 02:57:02 +0530

    Initialized skelton application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTp::Application.config.secret_token = '52bcf2ed2936566e4ad9824c092b3e67ef497e789a93dde06d400ba90449d120d50224dd9e899dd926276652fb7385c8ca94ebf545dc10488b4b52b9d419c983'
